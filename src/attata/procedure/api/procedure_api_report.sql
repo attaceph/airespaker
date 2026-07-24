c94b31012783efa108f412563085d09de6c3e387
attaceph <attaceph@protonmail.com>
2026-07-25 03:15:55 +0700

    Add files via upload

--- src/attata/procedure/api/procedure_api_report.sql
+++ src/attata/procedure/api/procedure_api_report.sql
@@ -18,14 +18,22 @@ begin
   set v_start_date = '2026-07-17';
   set v_week_start = 0; -- Monday
   set v_week_start = 1; -- Sunday
+  set v_week_start = 2; -- Saturday
+  set v_week_start = 0;
 
   select date(`created`) as `date`, count(`id`) as `register_count_day` from `ara_users` where date(`created`) >= v_start_date group by date(`created`) order by date(`created`) desc;
 
   select date(`created`) as `date`, count(`id`) as `responses_count_day` from `ara_responses` where date(`created`) >= v_start_date group by date(`created`) order by date(`created`) desc;
 
+  select date(r.`created`) as `date`, u.`username` as `username`, count(r.`id`) as `responses_count_day` from `ara_responses` r, `ara_users` u where r.`user_id` = u.`id` and date(r.`created`) >= v_start_date group by date(r.`created`), u.`username` order by date(r.`created`) desc;
+
   select date(`created` - interval (dayofweek(`created`) - v_week_start) day) as `date`, count(`id`) as `register_count_week` from `ara_users` where date(`created`) >= v_start_date group by date(`created` - interval (dayofweek(`created`) - v_week_start) day) order by date(`created` - interval (dayofweek(`created`) - v_week_start) day) desc;
 
   select date(`created` - interval (dayofweek(`created`) - v_week_start) day) as `date`, count(`id`) as `responses_count_week` from `ara_responses` where date(`created`) >= v_start_date group by date(`created` - interval (dayofweek(`created`) - v_week_start) day) order by date(`created` - interval (dayofweek(`created`) - v_week_start) day) desc;
+
+  select date(r.`created` - interval (dayofweek(r.`created`) - v_week_start) day) as `date`, u.`username` as `username`, count(r.`id`) as `responses_count_week` from `ara_responses` r, `ara_users` u where r.`user_id` = u.`id` and date(r.`created`) >= v_start_date group by date(r.`created` - interval (dayofweek(r.`created`) - v_week_start) day), u.`username` order by date(r.`created` - interval (dayofweek(r.`created`) - v_week_start) day) desc;
+
+  select date(`created`) as `date`, `username` as `username` from `ara_users` where date(`created`) >= v_start_date order by `created` desc;
 
 end;$$
 delimiter ;
